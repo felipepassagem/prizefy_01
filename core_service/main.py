10bc2d1a0d760f0d56a81dff33700e9276d8955c
felipepassagem <64990930+felipepassagem@users.noreply.github.com>
2025-04-19 20:31:01 -0300

    estrutura auth
    primeira estruturacao auth

---
+++ core_service/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from fastapi import FastAPI
+from auth.routes import router as auth_router
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "API Prizefy online"}
+
+
+
+app = FastAPI()
+app.include_router(auth_router, prefix="/auth")
